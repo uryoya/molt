ed4c3ed0aeee10cc2795ba02e049cfc288edb247
URANO Ryoya <uryoya.ur@gmail.com>
2017-02-12 19:26:49 +0900

    :new: add molt_core.py

---
+++ molt_core.py
@@ -0,0 +1,13 @@
+import subprocess
+
+
+def molt(rev, repo, user):
+    pass
+
+
+if __name__ == '__main__':
+    rev = '32b5b45cf39f2f49f340b664a85522059bc4fe0a'
+    repo = 'molt'
+    user = 'swkoubou'
+    result = molt(rev, repo, user)
+    print(result)
