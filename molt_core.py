5e2c45efd984879cb94e89e55a64feddfbc067a0
URANO Ryoya <uryoya.ur@gmail.com>
2017-02-12 20:44:07 +0900

    :construction: コマンドの実行と標準出力の取得

--- molt_core.py
+++ molt_core.py
@@ -1,13 +1,26 @@
 import subprocess
+import shlex
 
 
 def molt(rev, repo, user):
-    pass
+    """ gitリポジトリのクローンと、Dockerイメージの立ち上げ """
+    command = 'git clone --progress {} ./tmp'.format(   # コマンドの生成
+        git_ripository_url(repo, user))
+    args = shlex.split(command)     # コマンドをリストに分解
+    popen = subprocess.Popen(args,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT)
+    return popen.stdout
+
+
+def git_ripository_url(repo, user):
+    return 'https://github.com/{}/{}.git'.format(user, repo)
 
 
 if __name__ == '__main__':
     rev = '32b5b45cf39f2f49f340b664a85522059bc4fe0a'
     repo = 'molt'
     user = 'swkoubou'
-    result = molt(rev, repo, user)
-    print(result)
+
+    for line in molt(rev, repo, user):
+        print(line.decode(), end='', flush=True)
