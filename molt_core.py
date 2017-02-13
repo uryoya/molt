2d9fdcf68f92c3775154d50d8e6ec720cde9d2ed
URANO Ryoya <uryoya.ur@gmail.com>
2017-02-13 19:56:42 +0900

    :construction: composeコマンド部分の実装

--- molt_core.py
+++ molt_core.py
@@ -13,13 +13,14 @@ class Molt:
 
     def molt(self):
         """ gitリポジトリのクローンと、Dockerイメージの立ち上げ """
-        for command in (self._git_clone, self._git_checkout):
+        for command in (self._git_clone, self._git_checkout,
+                        self._compose_build, self._compose_up):
             for row in command().stdout:
                 yield row
 
     def _git_clone(self):
-        command = 'git clone --progress {} {}'.format(
-            self.repo_url, self.repo_dir)
+        command = 'git clone --progress {} {}'.format(self.repo_url,
+                                                      self.repo_dir)
         return subprocess.Popen(shlex.split(command),
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.STDOUT)
@@ -31,10 +32,24 @@ class Molt:
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.STDOUT)
 
+    def _compose_build(self):
+        command = 'compose build --no-cache'
+        return subprocess.Popen(shlex.split(command),
+                                cwd=self.repo_dir,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
+
+    def _compose_up(self):
+        command = 'compose up'
+        return subprocess.Popen(shlex.split(command),
+                                cwd=self.repo_dir,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
+
 
 if __name__ == '__main__':
-    rev = '32b5b45'
-    repo = 'molt'
+    rev = '2ea1328'
+    repo = 'swkoubou.rms'
     user = 'swkoubou'
 
     m = Molt(rev, repo, user)
