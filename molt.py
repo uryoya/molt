ba3ac9de4b40b4b776e955154a3cfeaa6aa7a30a
URANO Ryoya <uryoya.ur@gmail.com>
2017-03-06 13:31:14 +0900

    :art: 機能に準じた名前に変更

--- molt.py
+++ molt.py
@@ -1,74 +1,138 @@
-"""Molt Web API with Interface."""
-import re
-import redis
+"""Molt Core Interfaces."""
+import subprocess
+import shlex
+import docker
+import yaml
+import tempfile
 
-from flask import Flask, Response, render_template, abort
-from molt_core import Molt
-
-app = Flask(__name__)
-
-
-@app.route('/<virtual_host>')
-def index(virtual_host):
-    """Moltの実行をプレビューするページ."""
-    rev, repo, user = virtual_host_parse(virtual_host)
-    vhost = {'rev': rev, 'repo': repo, 'user': user}
-    return render_template('index.html', vhost=vhost)
+from pathlib import Path
 
 
-@app.route('/molt/<virtual_host>', methods=['GET'])
-def molt(virtual_host):
-    """Moltの実行をストリーミングする(Server-Sent Eventを使ったAPI)."""
-    rev, repo, user = virtual_host_parse(virtual_host)
-    m = Molt(rev, repo, user)
-    r = redis.StrictRedis()
+class Molt:
+    """指定されたvirtual_hostを元にDockerイメージの立ち上げ・管理をする."""
 
-    def generate(m, r):
-        """Dockerイメージ立ち上げ(ストリーミングするための関数).
+    def __init__(self, rev, repo, user):
+        """コンストラクタ."""
+        self.rev = rev
+        self.repo = repo
+        self.user = user
+        self.repo_url = 'https://github.com/{}/{}.git/'.format(user, repo)
+        self.repo_dir = str(Path('./repos') / user / repo / rev)
+        self.molt_yml_fp = None
 
-        git clone から docker-compose upまでの一連の処理のSTDIOの送信と、Dockerイメージ
-        の情報取得・設定をする
+    def __del__(self):
+        """デストラクタ."""
+        self.molt_yml_fp.close()
+
+    def molt(self):
+        """Gitリポジトリのクローンと、Dockerイメージの立ち上げ."""
+        for command in (self._git_clone, self._git_checkout,
+                        self._marge_docker_compose,
+                        self._compose_build, self._compose_up):
+            for row in command().stdout:
+                yield row
+
+    def get_container_ip(self):
+        """Moltで生成したコンテナのIPアドレスを取得する."""
+        client = docker.from_env()
+        container = client.containers.get(self.gen_container_name())
+        key = list(container.attrs['NetworkSettings']['Networks'].keys())[0]
+        return container.attrs['NetworkSettings']['Networks'][key]['IPAddress']
+
+    def gen_container_name(self):
+        """docker-compose.ymlで使用するcontainer_nameを生成する."""
+        molt_conf = self.get_molt_config_files()
+        return '-'.join([self.user, self.repo, self.rev, molt_conf['entry']])
+
+    def get_molt_config_files(self):
+        """molt-config.yml ファイルからmoltの設定を読み込む.
+
+        e.g. molt-config.yml:
+        MOLT: FILE: file1 file2 ... ENTRY: entryname
+        [EOF]
         """
-        # コマンド群の実行
-        for row in m.molt():
-            row = row.decode()
-            data = row.split('\r')[-1]    # CRのみの行は保留されるので取り除く
-            yield event_stream_parser(data)
-        # RedisへIPアドレスとバーチャルホストの対応を書き込む
-        r.hset('mirror-store', virtual_host, m.get_container_ip())
-    return Response(generate(m, r), mimetype='text/event-stream')
+        with open(self.repo_dir + '/molt-config.yml', 'r') as f:
+            s = f.read()
+        moltcfg = s.split()
+        files = moltcfg[moltcfg.index('FILE:') + 1:moltcfg.index('ENTRY:')]
+        entry = moltcfg[moltcfg.index('ENTRY:') + 1:][0]
+        return {'file': files, 'entry': entry}
 
+    # 以下はSHELLで実行するコマンドの記述
+    def _git_clone(self):
+        command = 'git clone --progress {} {}'.format(self.repo_url,
+                                                      self.repo_dir)
+        return subprocess.Popen(shlex.split(command),
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
 
-@app.route('/favicon.ico')
-def favicon():
-    """favicon.ico."""
-    abort(404)
+    def _git_checkout(self):
+        command = 'git checkout {}'.format(self.rev)
+        return subprocess.Popen(shlex.split(command),
+                                cwd=self.repo_dir,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
 
+    def _marge_docker_compose(self):
+        """Molt用にdocker-compose.ymlを統合して書き換える."""
+        molt_conf = self.get_molt_config_files()
+        compose_files = molt_conf['file']
+        data = {}
+        for filename in compose_files:
+            with open(str(Path(self.repo_dir) / filename), 'r') as f:
+                data.update(yaml.load(f))    # 各yamlファイルを統合・上書き
+        molt_conf = self.get_molt_config_files()
+        # container_nameの追加
+        data['services']['web']['container_name'] = self.gen_container_name()
+        # 変更したcompose fileの書き出し
+        fp = tempfile.NamedTemporaryFile(mode='w')
+        yaml.dump(data, fp)
+        self.molt_yml_fp = fp
 
-def virtual_host_parse(virtual_host):
-    """Virtual_hostの文字列を 'rev', 'repo', 'user' に分割する.
+        # メソッドの形式を同じにするためにsubprocessを使用
+        command = 'echo "moltの設定ファイルを生成中..."'
+        return subprocess.Popen(shlex.split(command),
+                                cwd=self.repo_dir,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
 
-    e.g.(1) "host.repo.sitory.user" => "host", "repo.sitory", "user"
-    e.g.(2) "host.repository.user" => "host", "repository", "user"
-    """
-    p = re.compile(r'(?P<rev>^.+?)\.(?P<repo>.+)\.(?P<user>.+)$')
-    m = p.search(virtual_host)
-    return m.group('rev'), m.group('repo'), m.group('user')
+    def _compose_build(self):
+        molt_conf = self.get_molt_config_files()
+        compose_files = molt_conf['file']
+        if compose_files == []:
+            command = 'docker-compose build --no-cache'
+        else:
+            compose_files.append(self.molt_yml_fp.name)
+            expand_conf = '-f {} '*len(compose_files)
+            expand_conf = expand_conf.format(*compose_files)
+            command = 'docker-compose ' + expand_conf + 'build --no-cache'
+        return subprocess.Popen(shlex.split(command),
+                                cwd=self.repo_dir,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
 
-
-def event_stream_parser(data, event=None, id=None, retry=None):
-    """Server-Sent Event 形式へのパーサ."""
-    event_stream = ''
-    if event:
-        event_stream += 'event: {}\n'.format(event)
-    event_stream += 'data: {}\n'.format(data)
-    if id:
-        event_stream += 'id: {}\n'.format(id)
-    if retry:
-        event_stream += 'retry: {}\n'.format(id)
-    event_stream += '\n'
-    return event_stream
+    def _compose_up(self):
+        molt_conf = self.get_molt_config_files()
+        compose_files = molt_conf['file']
+        if compose_files == []:
+            command = 'docker-compose up -d'
+        else:
+            compose_files.append(self.molt_yml_fp.name)
+            expand_conf = '-f {} '*len(compose_files)
+            expand_conf = expand_conf.format(*compose_files)
+            command = 'docker-compose ' + expand_conf + 'up -d'
+        return subprocess.Popen(shlex.split(command),
+                                cwd=self.repo_dir,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    rev = '4809f18'
+    repo = 'molt-test'
+    user = 'swkoubou'
+
+    m = Molt(rev, repo, user)
+    for line in m.molt():
+        print(line.decode(), end='', flush=True)
+    print()
---
+++ molt_app.py
@@ -0,0 +1,74 @@
+"""Molt Web API with Interface."""
+import re
+import redis
+
+from flask import Flask, Response, render_template, abort
+from molt import Molt
+
+app = Flask(__name__)
+
+
+@app.route('/<virtual_host>')
+def index(virtual_host):
+    """Moltの実行をプレビューするページ."""
+    rev, repo, user = virtual_host_parse(virtual_host)
+    vhost = {'rev': rev, 'repo': repo, 'user': user}
+    return render_template('index.html', vhost=vhost)
+
+
+@app.route('/molt/<virtual_host>', methods=['GET'])
+def molt(virtual_host):
+    """Moltの実行をストリーミングする(Server-Sent Eventを使ったAPI)."""
+    rev, repo, user = virtual_host_parse(virtual_host)
+    m = Molt(rev, repo, user)
+    r = redis.StrictRedis()
+
+    def generate(m, r):
+        """Dockerイメージ立ち上げ(ストリーミングするための関数).
+
+        git clone から docker-compose upまでの一連の処理のSTDIOの送信と、Dockerイメージ
+        の情報取得・設定をする
+        """
+        # コマンド群の実行
+        for row in m.molt():
+            row = row.decode()
+            data = row.split('\r')[-1]    # CRのみの行は保留されるので取り除く
+            yield event_stream_parser(data)
+        # RedisへIPアドレスとバーチャルホストの対応を書き込む
+        r.hset('mirror-store', virtual_host, m.get_container_ip())
+    return Response(generate(m, r), mimetype='text/event-stream')
+
+
+@app.route('/favicon.ico')
+def favicon():
+    """favicon.ico."""
+    abort(404)
+
+
+def virtual_host_parse(virtual_host):
+    """Virtual_hostの文字列を 'rev', 'repo', 'user' に分割する.
+
+    e.g.(1) "host.repo.sitory.user" => "host", "repo.sitory", "user"
+    e.g.(2) "host.repository.user" => "host", "repository", "user"
+    """
+    p = re.compile(r'(?P<rev>^.+?)\.(?P<repo>.+)\.(?P<user>.+)$')
+    m = p.search(virtual_host)
+    return m.group('rev'), m.group('repo'), m.group('user')
+
+
+def event_stream_parser(data, event=None, id=None, retry=None):
+    """Server-Sent Event 形式へのパーサ."""
+    event_stream = ''
+    if event:
+        event_stream += 'event: {}\n'.format(event)
+    event_stream += 'data: {}\n'.format(data)
+    if id:
+        event_stream += 'id: {}\n'.format(id)
+    if retry:
+        event_stream += 'retry: {}\n'.format(id)
+    event_stream += '\n'
+    return event_stream
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
